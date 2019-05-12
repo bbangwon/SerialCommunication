460344a8fcfaeeb64675995e98888450df6ab362
bbangwon <bbangwon.chung@gmail.com>
2019-05-12 10:18:36 +0900

    SerialCommunication

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace SerialCommunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbSettings = new System.Windows.Forms.GroupBox();
+            this.cbHandshake = new System.Windows.Forms.ComboBox();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.cbData = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbBaudrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.sendTimer = new System.Windows.Forms.Timer(this.components);
+            this.gbSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbSettings
+            // 
+            this.gbSettings.Controls.Add(this.cbHandshake);
+            this.gbSettings.Controls.Add(this.cbParity);
+            this.gbSettings.Controls.Add(this.cbData);
+            this.gbSettings.Controls.Add(this.label5);
+            this.gbSettings.Controls.Add(this.label4);
+            this.gbSettings.Controls.Add(this.label3);
+            this.gbSettings.Controls.Add(this.cbBaudrate);
+            this.gbSettings.Controls.Add(this.label2);
+            this.gbSettings.Controls.Add(this.cbPort);
+            this.gbSettings.Controls.Add(this.label1);
+            this.gbSettings.Location = new System.Drawing.Point(12, 12);
+            this.gbSettings.Name = "gbSettings";
+            this.gbSettings.Size = new System.Drawing.Size(355, 249);
+            this.gbSettings.TabIndex = 0;
+            this.gbSettings.TabStop = false;
+            this.gbSettings.Text = "Settings";
+            // 
+            // cbHandshake
+            // 
+            this.cbHandshake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbHandshake.FormattingEnabled = true;
+            this.cbHandshake.Items.AddRange(new object[] {
+            "none",
+            "Xon/Xoff",
+            "request to send",
+            "request to send Xon/Xoff"});
+            this.cbHandshake.Location = new System.Drawing.Point(137, 190);
+            this.cbHandshake.Name = "cbHandshake";
+            this.cbHandshake.Size = new System.Drawing.Size(190, 26);
+            this.cbHandshake.TabIndex = 9;
+            // 
+            // cbParity
+            // 
+            this.cbParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Items.AddRange(new object[] {
+            "none",
+            "even",
+            "mark",
+            "odd",
+            "space"});
+            this.cbParity.Location = new System.Drawing.Point(137, 150);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(190, 26);
+            this.cbParity.TabIndex = 8;
+            // 
+            // cbData
+            // 
+            this.cbData.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbData.FormattingEnabled = true;
+            this.cbData.Items.AddRange(new object[] {
+            "8",
+            "7",
+            "6"});
+            this.cbData.Location = new System.Drawing.Point(137, 110);
+            this.cbData.Name = "cbData";
+            this.cbData.Size = new System.Drawing.Size(190, 26);
+            this.cbData.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "handshake";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "parity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "data";
+            // 
+            // cbBaudrate
+            // 
+            this.cbBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaudrate.FormattingEnabled = true;
+            this.cbBaudrate.Items.AddRange(new object[] {
+            "115200",
+            "19200",
+            "38400",
+            "57600",
+            "9600"});
+            this.cbBaudrate.Location = new System.Drawing.Point(137, 70);
+            this.cbBaudrate.Name = "cbBaudrate";
+            this.cbBaudrate.Size = new System.Drawing.Size(190, 26);
+            this.cbBaudrate.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "baudrate";
+            // 
+            // cbPort
+            // 
+            this.cbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(137, 30);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(190, 26);
+            this.cbPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 279);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(355, 56);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Message";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(386, 36);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbMessage.Size = new System.Drawing.Size(355, 225);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(386, 279);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(355, 56);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "반복 0xFF 보내기";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // sendTimer
+            // 
+            this.sendTimer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 347);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.gbSettings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "SerialCommunication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbSettings.ResumeLayout(false);
+            this.gbSettings.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbSettings;
+        private System.Windows.Forms.ComboBox cbHandshake;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.ComboBox cbData;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbBaudrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer sendTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialCommunication
+{
+    public partial class Form1 : Form
+    {
+        enum LogType
+        {
+            System,
+            Send,
+            Receive
+        }
+
+        private SerialPort _Port;
+
+        private SerialPort Port
+        {
+            get
+            {
+                if(_Port == null)
+                {
+                    _Port = new SerialPort();
+                    _Port.PortName = "COM1";
+                    _Port.BaudRate = 9600;
+                    _Port.DataBits = 8;
+                    _Port.Parity = Parity.None;
+                    _Port.Handshake = Handshake.None;
+                    _Port.StopBits = StopBits.One;
+                    _Port.DataReceived += _Port_DataReceived;
+                }
+                return _Port;
+            }
+        }
+
+        private Boolean IsOpen
+        {
+            get => Port.IsOpen;
+            set
+            {
+                if(value)
+                {
+                    Log(LogType.System, "연결 됨");
+                    btnConnect.Text = "연결 끊기";
+                    btnSend.Enabled = true;
+                    gbSettings.Enabled = false;                    
+                }
+                else
+                {
+                    Log(LogType.System, "연결 해제됨");
+                    btnConnect.Text = "연결";
+                    btnSend.Enabled = false;
+                    gbSettings.Enabled = true;
+                }
+            }
+        }
+
+        private StringBuilder _String;
+        private String Strings
+        {
+            set
+            {
+                if (_String == null)
+                    _String = new StringBuilder(1024);
+
+                if (_String.Length >= (1024 - value.Length))
+                    _String.Clear();
+
+                _String.AppendLine(value);
+                tbMessage.Text = _String.ToString();
+                tbMessage.Select(tbMessage.Text.Length, 0);
+                tbMessage.ScrollToCaret();
+            }
+        }
+
+        private bool _timer = false;
+        
+
+        void Log(LogType logType, string formatString, params object[] objs)
+        {
+            string sLogType = logType.ToString();
+            string sLogTime = DateTime.Now.ToString("H:mm:ss.fff");
+
+            string sLog = string.Format(formatString, objs);
+
+            Strings = string.Format("[{0}] ({1}) {2}", sLogType, sLogTime, sLog);
+        }
+
+        private void _Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String msg = Port.ReadExisting();
+            this.Invoke(new EventHandler(delegate
+            {
+                Log(LogType.Receive, msg);
+            }));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbPort.DataSource = SerialPort.GetPortNames();
+
+            cbBaudrate.SelectedIndex = 0;
+            cbData.SelectedIndex = 0;
+            cbParity.SelectedIndex = 0;
+            cbHandshake.SelectedIndex = 0;
+
+            sendTimer.Tick += SendTimer_Tick;
+        }
+
+        private void SendTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Port.Write(new byte[] { 0xFF }, 0, 1);
+                Log(LogType.Send, "0xFF");
+            }
+            catch(Exception ex)
+            {
+                Log(LogType.System, ex.Message);
+            }           
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if(!Port.IsOpen)
+            {
+                Port.PortName = cbPort.SelectedItem.ToString();
+                Port.BaudRate = Convert.ToInt32(cbBaudrate.SelectedItem);
+                Port.DataBits = Convert.ToInt32(cbData.SelectedItem);
+                Port.Parity = (Parity)cbParity.SelectedIndex;
+                Port.Handshake = (Handshake)cbHandshake.SelectedIndex;
+
+                try
+                {
+                    Port.Open();
+                }
+                catch (Exception ex) { Log(LogType.System, ex.Message); }                              
+            }
+            else
+            {
+                TimerStop();
+                Port.Close();
+            }
+
+            IsOpen = Port.IsOpen;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(!_timer)
+            {
+                _timer = true;
+                btnSend.Text = "보내기 중지";
+                Log(LogType.System, "보내기 시작");
+                sendTimer.Start();
+                return;
+            }
+
+            TimerStop();
+        }
+
+        void TimerStop()
+        {
+            if(_timer)
+            {
+                _timer = false;
+                btnSend.Text = "반복 0xFF 보내기";
+                Log(LogType.System, "보내기 중지");
+                sendTimer.Stop();
+            }
+        }
+    }
+}
